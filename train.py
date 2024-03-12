439d4dff112c558aa14ba8e514c355e59f083a26
kanayama <japan.tokai.k.r@gmail.com>
2024-03-13 05:50:27 +0900

    Separate showing results function

--- train.py
+++ train.py
@@ -5,9 +5,7 @@ import torch
 from references.detection.engine import train_one_epoch, evaluate
 from references.detection import utils, coco_eval, coco_utils
 from utils_pkg import PennFudanDataset, get_transform, get_model_instance_segmentation
-import matplotlib.pyplot as plt
-from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
-from torchvision.io import read_image
+
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -56,33 +54,5 @@ for epoch in range(num_epochs):
 
 print("That's it!")
 
-
-
-"""
-結果の確認
-"""
-image = read_image("data/PennFudanPed/PNGImages/FudanPed00001.png")
-eval_transform = get_transform(train=False)
-
-model.eval()
-with torch.no_grad():
-    x = eval_transform(image)
-    # convert RGBA -> RGB and move to device
-    x = x[:3, ...].to(device)
-    predictions = model([x, ])
-    pred = predictions[0]
-
-image = (255.0 * (image - image.min()) / (image.max() - image.min())).to(torch.uint8)
-image = image[:3, ...]
-pred_labels = [f"pedestrian: {score:.3f}" for label, score in zip(pred["labels"], pred["scores"])]
-pred_boxes = pred["boxes"].long()
-output_image = draw_bounding_boxes(image, pred_boxes, pred_labels, colors="red")
-
-masks = (pred["masks"] > 0.7).squeeze(1)
-output_image = draw_segmentation_masks(output_image, masks, alpha=0.5, colors="blue")
-
-plt.figure(figsize=(12, 12))
-plt.imshow(output_image.permute(1, 2, 0))
-plt.savefig("results/output_image_hepler_v.png")
-
-
+# save the model
+torch.save(model.state_dict(), "results/mask_r_cnn.pth")
